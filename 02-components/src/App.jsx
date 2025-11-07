c8c9e49594ff105d71d37913abfc587abc6c9729
vivek-gamit <vgamit637@gmail.com>
2025-11-07 15:38:05 +0530

    components in react

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./component/card"
+
+function App() {
+  return (
+    <div>
+       <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/component/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function card() {
+  return (
+   <div className="card">
+        <h2>ViveK Gamit</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, hic.</p>
+    </div>
+  )
+}
+
+export default card
