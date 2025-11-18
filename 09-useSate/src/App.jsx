41024a9d7827b977fc8b480011b9f3cc3ed4ba7f
vivek-gamit <vgamit637@gmail.com>
2025-11-18 15:03:44 +0530

    counter using reactjs

--- 09-useSate/src/App.jsx
+++ 09-useSate/src/App.jsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react'
 
-
 function App() {
+  const [num, setnum] = useState(0)
 
-  const [num, setnum] = useState(20)
-  const [name, setname] = useState('Vivek')
-
-  function ChangeNum(){
-    setnum(30)
-    setname('Asus')
+  function IncreseNum(){
+    console.log('Incresing num')
+    setnum(num+1)
+  }
+  function DecreseNum(){
+    console.log('Decresing num')
+    setnum(num-1)
+  }
+  function Jump5Num(){
+    console.log('Changing Num By 5')
+    setnum(num+5)
+  }
+  function Default(){
+    console.log('Set Default Value 0')
+    setnum(0)
   }
   return (
     <div>
-      <h1>Value of num {num} <br />Hello user {name}</h1>
-      <button onClick={ChangeNum}>Click</button>
+      <h1>{num}</h1>
+      <button onClick={IncreseNum}>Increse +</button>
+      <button onClick={DecreseNum}>Decrese -</button><br />
+      <button onClick={Jump5Num}>Jump By 5</button>
+      <button onClick={Default}>Default</button>
     </div>
   )
 }
